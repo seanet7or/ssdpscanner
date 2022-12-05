b0a5fe322dc6fc4da944078ec27835ae93f60739
Benjamin Caspari <becaspari@gmail.com>
2022-12-05 21:34:53 +0100

    created maui app

---
+++ SsdpScanner/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SsdpScanner;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SsdpScanner/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SsdpScanner;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
