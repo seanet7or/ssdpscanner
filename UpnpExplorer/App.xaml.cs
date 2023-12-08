38280fa76a4644f30c4deadf1079fdc65676bc75
Benjamin Caspari <becaspari@gmail.com>
2023-12-08 21:47:10 +0100

    improved layout

--- UpnpExplorer/App.xaml.cs
+++ UpnpExplorer/App.xaml.cs
@@ -8,5 +8,22 @@
 
             MainPage = new AppShell();
         }
+
+#if WINDOWS
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+
+            const int newWidth = 600;
+            var newHeight = Math.Min(1000, displayInfo.Height / displayInfo.Density);
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
+#endif
     }
 }
